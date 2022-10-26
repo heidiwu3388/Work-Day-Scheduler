ae595dde37be343844f5749639770f70589858fe
heidiwu3388 <heidiwu3388@yahoo.com>
2022-10-25 22:43:45 -0700

    set time block color base on whether it is past, present or future

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,42 @@
 // get elements and save them in variables
 var currentDateEl = $("#currentDay");
+var containerEl = $(".container");
 
 
 // get current Date and hour
 var currentDate = moment().format("dddd, MMMM Do");
 
 
+//****** for testing purpose ******/
+//var currentHour = parseInt(moment().format("H"));
+currentHour = 12; 
+//****** for testing purpose ******/
+
+console.log(currentHour);
+function setTimeBlockStyle() {
+    // loop for each time block (row) in the container
+    for (i=0; i<containerEl.children().length; i++ ) {
+        // get the time block hour
+        let thisRow = containerEl.children().eq(i);
+        timeBlockHour = parseInt(thisRow.attr("data-hour"));
+        // set styling class base on whether the time block is past, present or future
+        if (timeBlockHour < currentHour) {
+            console.log("time block hour: ", timeBlockHour, "(past)");
+            thisRow.children("textarea").addClass("past");
+        } else if (timeBlockHour > currentHour) {
+            console.log("time block hour: ", timeBlockHour, "(future)");
+            thisRow.children("textarea").addClass("future");
+        } else {
+            console.log("time block hour: ", timeBlockHour, "(present)");
+            thisRow.children("textarea").addClass("present");
+        }
+
+    }
+}
+
+
+
 // display current date in header
 currentDateEl.text(currentDate);
+// apply different style base on whether the time block is past, present or future
+setTimeBlockStyle();
