e6bc9a4b235581f0a332d99d4249a89239f1a5d8
heidiwu3388 <heidiwu3388@yahoo.com>
2022-10-28 22:10:11 -0700

    remove console.log()

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,7 +21,6 @@ const schedule = {
 
 
 
-console.log(currentHour);
 function setTimeBlockStyle() {
     // loop for each time block (row) in the container
     for (let i=0; i<containerEl.children().length; i++ ) {
@@ -30,25 +29,20 @@ function setTimeBlockStyle() {
         let timeBlockHour = parseInt(thisRow.attr("data-hour"));
         // set styling class and disable textarea base on whether the time block is past, present or future
         if (timeBlockHour < currentHour) {
-            console.log("time block hour: ", timeBlockHour, "(past)");
             thisRow.children("textarea").addClass("past").attr("disabled", true);
             thisRow.children(".saveBtn").text("");
         } else if (timeBlockHour === currentHour) {
-            console.log("time block hour: ", timeBlockHour, "(present)");
             thisRow.children("textarea").addClass("present").attr("disabled", true);
             thisRow.children(".saveBtn").text("");
         } else {
-            console.log("time block hour: ", timeBlockHour, "(future)");
             thisRow.children("textarea").addClass("future");
         }
     }
 }
 
 function loadData() {
-    console.log ("load data");
     // get previous schedule from local storage
     let prevSchedule = JSON.parse(localStorage.getItem("schedule"));
-    console.log("prevSchedule : \n", prevSchedule);
     // if previous schedule exist and the date is today
     if (prevSchedule !== null && prevSchedule.date === currentDate) {
         // update the schedule object with data from local storage
@@ -57,16 +51,13 @@ function loadData() {
         for (let i=0; i<schedule.allHourTask.length; i++) {
                 let hour = schedule.allHourTask[i].hour;
                 let task = schedule.allHourTask[i].task;
-                console.log ("hour and task: ", hour, task);
                 let selector = `div[data-hour='${hour}']`;
-                console.log("selector: ", selector);
                 // containerEl.children(selector).children("textarea").val = task;
                 containerEl.children(selector).children("textarea").val(task);
             }
             // children("textarea").val = task;
         
     }
-    console.log("schedule: ", schedule);
     return;
 }
 
@@ -75,41 +66,33 @@ function saveTask(event) {
     let textEl = clickedBtnEl.parent().children("textarea");
     // do nothing for past and present time block
     if (textEl.hasClass("past") || textEl.hasClass("present")) {
-        console.log("past or present save clicked")
         return; 
     }
-    console.log("future save clicked");
     // get the hour and task from the clicked row
     let hour = parseInt(clickedBtnEl.parent().attr("data-hour"));
     let task = textEl.val().trim();
-    console.log("hour: ", hour);
-    console.log("task: ", task);
     // look for the hour block in the schedule object
     found = false;
     for (let i=0; i<schedule.allHourTask.length; i++){
         // if the hour block is found, update it with new task
         if (schedule.allHourTask[i].hour === hour) {
             found = true;
-            console.log("found");
             schedule.allHourTask[i].task = task;
         }
     }
     // if the hour block is not found in the schedule object
     if (!found){
-        console.log("not found");
         // create a new time block with current hour and task
         const hourTask = {
             hour: hour,
             task: task
         }
-        console.log("hourTask : ", hourTask);
         // append the new time block to the schedule object
         schedule.allHourTask.push(hourTask);
         
     }
     
     // store the schedule object into the local storage
-    console.log("schedule : ", schedule);
     localStorage.setItem("schedule", JSON.stringify(schedule));
     clickedBtnEl.text("saved");
 }
